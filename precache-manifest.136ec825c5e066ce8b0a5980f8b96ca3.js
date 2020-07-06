c00c35669e53135191efb06b36baa68cbb7cf716
sryklief <sryklief@users.noreply.github.com>
2020-07-06 14:38:16 +0200

    Updates

---
+++ precache-manifest.136ec825c5e066ce8b0a5980f8b96ca3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b65410ad734857346a8709e75da3cad",
+    "url": "/myPortfolio/index.html"
+  },
+  {
+    "revision": "c2eec344cb6844c16c15",
+    "url": "/myPortfolio/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "b276e80b9d9d0d130503",
+    "url": "/myPortfolio/static/css/main.c67d1a77.chunk.css"
+  },
+  {
+    "revision": "c2eec344cb6844c16c15",
+    "url": "/myPortfolio/static/js/2.5c9966c3.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/myPortfolio/static/js/2.5c9966c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b276e80b9d9d0d130503",
+    "url": "/myPortfolio/static/js/main.655dd130.chunk.js"
+  },
+  {
+    "revision": "1ad0ff881b619ec623bf",
+    "url": "/myPortfolio/static/js/runtime-main.975055d4.js"
+  },
+  {
+    "revision": "9422ad0a8d74f4194ab0a12658e45e3f",
+    "url": "/myPortfolio/static/media/picme.9422ad0a.jpg"
+  }
+]);
